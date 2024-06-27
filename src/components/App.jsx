f416ef7d540b2e33aa4a81a9a57fa78875ebdece
SaraSmarandoiu <smarandoiusara@gmail.com>
2024-06-27 21:44:25 +0300

    commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,40 @@
+import React from 'react';
+import Profile from './Profile/profile';
+import Statistics from './Statistics/statistics';
+import FriendList from './FriendList/friendList';
+import TransactionHistory from './TransactionHistory/transactionHistory';
+import user from './Profile/user';
+import data from './Statistics/data';
+import friends from './FriendList/friends';
+import transactions from './TransactionHistory/transactions';
+
 export const App = () => {
   return (
     <div
       style={{
-        height: '100vh',
+        height: '250vh',
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
+        flexDirection: 'column',
       }}
     >
-      React homework template
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+
+      <Statistics title="Upload stats" stats={data} />
+      <Statistics stats={data} />
+
+      <FriendList friends={friends} />
+
+      <TransactionHistory items={transactions} />
     </div>
   );
 };
